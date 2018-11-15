14c05923833bf29f5ba61b6ef518613fdd04cce3
ForAl1830446 <foral1830446@etu.cegepjonquiere.ca>
2018-11-15 10:48:03 -0500

    Exo tableau joueur function

--- ExercicesJs/Exercice _ Joueurs/JS/traitement.js
+++ ExercicesJs/Exercice _ Joueurs/JS/traitement.js
@@ -8,6 +8,127 @@ var points;
 function btnAjouter_onclick()
 {
 
+    Ajoute();
+}
+function btnTrouverMoy_onclick()
+{
+    res = Moy();
+
+
+}
+
+function btnTrouverMeilleur_onclick()
+
+{
+    aff = Meilleur();
+}
+
+function btnTrouverPire_onclick()
+
+{
+   resultat = Pire();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Moy(){
+    var moy = 0;
+    var Tot=0;
+    var res;
+    for(var i = 0;i<TabPoints.length;i++)
+    {
+
+        Tot += TabPoints[i];
+
+    }
+
+    moy = Tot/4;
+    res=document.getElementById("lblReponse").innerHTML= ("Réponse "+ moy);
+    return res;
+}
+
+
+
+
+
+function Pire(){
+    var resultat;
+    var min = TabPoints[0];
+    var nomPire = TabJoueur[0];
+    for(var i = 0;i<TabPoints.length;i++)
+    {
+
+        if(min >TabPoints[i])
+        {
+            min = TabPoints[i];
+            nomPire = TabJoueur[i];
+        }
+
+    }
+
+
+    resultat= document.getElementById("lblReponse").innerHTML= "Réponse "+ min+ " Joueur "+nomPire;
+    return resultat;
+}
+
+
+
+function Meilleur(){
+    var max = TabPoints[0];
+    var nomMeilleur = TabJoueur[0];
+    var aff;
+    for(var i = 0;i<TabPoints.length;i++)
+    {
+
+        if(max <TabPoints[i])
+        {
+            max = TabPoints[i];
+            nomMeilleur = TabJoueur[i];
+        }
+
+    }
+
+
+    aff=document.getElementById("lblReponse").innerHTML= "Réponse "+ max +" Joueur "+nomMeilleur;
+    return aff;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Ajoute()
+{
     saisi=(document.getElementById("txtNom").value);
     points=parseFloat(document.getElementById("txtPoints").value);
     TabJoueur[cpt]= saisi;
@@ -24,100 +145,52 @@ function btnAjouter_onclick()
         document.getElementById("btnTrouverPire").disabled=false;
         document.getElementById("btnRechercher").disabled=false;
     }
-
-
-
-
-
-}
-
-
-function btnTrouverMoy_onclick()
-{
-    var moy = 0;
-    var Tot=0;
-
-    for(var i = 0;i<TabPoints.length;i++)
-    {
-
-        Tot += TabPoints[i];
-
-    }
-
-    moy = Tot/4;
-    document.getElementById("lblReponse").innerHTML= ("Réponse "+ moy);
-
-
-
-}
-
-
-function btnTrouverMeilleur_onclick()
-
-{
-
-    var max = TabPoints[0];
-    var nomMeilleur = TabJoueur[0];
-
-    for(var i = 0;i<TabPoints.length;i++)
-    {
-
-        if(max <TabPoints[i])
-        {
-            max = TabPoints[i];
-            nomMeilleur = TabJoueur[i];
-        }
-
-    }
-
-
-    document.getElementById("lblReponse").innerHTML= "Réponse "+ max +" Joueur "+nomMeilleur;
-
-
 }
 
 
 
-function btnTrouverPire_onclick()
-
-{
-    var min = TabPoints[0];
-    var nomPire = TabJoueur[0];
-    for(var i = 0;i<TabPoints.length;i++)
-    {
-
-        if(min >TabPoints[i])
-        {
-            min = TabPoints[i];
-            nomPire = TabJoueur[i];
-        }
-
-    }
 
 
-    document.getElementById("lblReponse").innerHTML= "Réponse "+ min+ " Joueur "+nomPire;
 
-}
+
+
+
+
+
+
+
+
 
 function btnRechercher_onclick() {
 
-    cpt = 0;
-    var recherche = TabJoueur[cpt];
-    var score = TabPoints[cpt];
+           var trouve;
+           var NoEtu;
+           var NomEtu;
+           var Points = 0;
+           trouve = false;
+           NoEtu = 0;
+           NomEtu = document.getElementById("txtNom").value;
 
-    do {
+           while(trouve == false && NomEtu <= 3)
+           {
 
-        cpt = -1;
-        cpt++;
+               if (TabJoueur[NoEtu] == NomEtu)
+               {
+                  trouve = true;
+                  Points = TabPoints[NoEtu];
+               }
 
+               else {
+                    NoEtu++;
+               }
 
-        if (recherche == TabJoueur[cpt] && recherche == TabJoueur[cpt] && recherche == TabJoueur[cpt] && recherche == TabJoueur[cpt]) {
-            document.getElementById("lblReponse").innerHTML = "Réponse " + score + " Joueur " + recherche;
+           }
 
-
-        }
-
-
-    } while (recherche != TabJoueur[cpt] && recherche != TabJoueur[cpt] && recherche != TabJoueur[cpt] && recherche != TabJoueur[cpt]);
+           if (trouve == true){
+               document.getElementById("lblReponse").innerHTML= "Réponse point joueur = "+Points+" Joueur "+NomEtu;
+           }
+           else{
+               document.getElementById("lblReponse").innerHTML= "Réponse   Joueur introuvable ";
+           }
 
 }
